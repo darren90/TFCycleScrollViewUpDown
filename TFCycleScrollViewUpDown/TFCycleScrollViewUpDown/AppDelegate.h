0c8b17e7fccff8dc9afad87d77a60cd5a3b74e8c
darren90 <1005052145@qq.com>
2016-05-15 11:22:28 +0800

    add

---
+++ TFCycleScrollViewUpDown/TFCycleScrollViewUpDown/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TFCycleScrollViewUpDown
+//
+//  Created by Tengfei on 16/5/15.
+//  Copyright © 2016年 tengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TFCycleScrollViewUpDown/TFCycleScrollViewUpDownTests/TFCycleScrollViewUpDownTests.m
@@ -0,0 +1,39 @@
+//
+//  TFCycleScrollViewUpDownTests.m
+//  TFCycleScrollViewUpDownTests
+//
+//  Created by Tengfei on 16/5/15.
+//  Copyright © 2016年 tengfei. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TFCycleScrollViewUpDownTests : XCTestCase
+
+@end
+
+@implementation TFCycleScrollViewUpDownTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
